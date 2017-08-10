3ef760a8ff0b54c9ac58a8f9255e28513566f0fa
jisu choi <h41469921@gmail.com>
2017-08-10 11:50:04 +0900

    mysql schedule insert , select , delete 기능 추가

--- mysqldb/views.py
+++ mysqldb/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from scheduler.spreadsheets import *
 import pymysql.cursors
 import json
 
@@ -23,3 +24,40 @@ def mysql_index(request):
         result = dict(msg=1)
         return HttpResponse(json.dumps(result), content_type="application/json")
     return render(request, 'view/mysql.html')
+
+
+def insert_schedule_mysql(request):
+    conn = pymysql.connect(host='localhost', user='root', password='1234', db='pymysql', charset='utf8mb4')
+    cur = conn.cursor()
+    schedule = analysis_sheet(spreadsheetId='1qEtfTDg0jn72MLDhmrUxFpJD7tzFyq-5qTySGvghKqg', channel='tvN')
+    for data in schedule:
+        sql = "INSERT INTO `schedule` (`channel`, `program_title`, `start_time`, `end_time`) VALUES (%s, %s, %s, %s)"
+        cur.execute(sql, (data['Channel'], data['ProgramTitle'], data['StartTime'], data['EndTime'],))
+    conn.commit()
+    cur.close()
+    conn.close()
+    result = dict(msg=1)
+    return HttpResponse(json.dumps(result), content_type="application/json")
+
+
+def get_schedule_mysql(request):
+    conn = pymysql.connect(host='localhost', user='root', password='1234', db='pymysql', charset='utf8mb4')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM schedule")
+    for row in cur:
+        print(row)
+    cur.close()
+    conn.close()
+    result = dict(msg=1)
+    return HttpResponse(json.dumps(result), content_type="application/json")
+
+
+def delete_schedule_mysql(request):
+    conn = pymysql.connect(host='localhost', user='root', password='1234', db='pymysql', charset='utf8mb4')
+    cur = conn.cursor()
+    cur.execute("DELETE FROM schedule")
+    conn.commit()
+    cur.close()
+    conn.close()
+    result = dict(msg=1)
+    return HttpResponse(json.dumps(result), content_type="application/json")
