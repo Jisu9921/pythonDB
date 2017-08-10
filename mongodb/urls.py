9da690e984c503b8f5d562dd42675ec97a7b1a10
jisu choi <h41469921@gmail.com>
2017-08-10 12:17:31 +0900

    mongodb schedule insert , select , delete 기능 추가

--- mongodb/urls.py
+++ mongodb/urls.py
@@ -1,7 +1,8 @@
 import django.conf.urls
-
-from .views import mongo_index
+from .views import *
 
 urlpatterns = [
-    django.conf.urls.url(r'^$', mongo_index, name='mongo_index'),
+    django.conf.urls.url(r'^insert_schedule/$', insert_schedule_mongo, name='insert_schedule'),
+    django.conf.urls.url(r'^get_schedule/$', get_schedule_mongo, name='get_schedule'),
+    django.conf.urls.url(r'^delete_schedule/$', delete_schedule_mongo, name='delete_schedule'),
 ]
--- mongodb/views.py
+++ mongodb/views.py
@@ -1,18 +1,37 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from scheduler.spreadsheets import *
 from pymongo import MongoClient
 import json
+client_mongo = MongoClient('localhost', 27017)
+mongo_db = client_mongo.pymongo
 
 
-def mongo_index(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        client_mongo = MongoClient('localhost', 27017)
-        mongo_db = client_mongo.pymongo
-        user = mongo_db.user
-        user_info = {"name": name}
-        user.insert_one(user_info).inserted_id
-        result = dict(msg=1)
-        return HttpResponse(json.dumps(result), content_type="application/json")
-    return render(request, 'view/mongo.html')
+def insert_schedule_mongo(request):
+    schedule = mongo_db.schedule
+    sheet_schedules = analysis_sheet(spreadsheetId='1qEtfTDg0jn72MLDhmrUxFpJD7tzFyq-5qTySGvghKqg', channel='tvN')
+    for data in sheet_schedules:
+        schedule_info = {
+            "channel": data['Channel'],
+            "program_title": data['ProgramTitle'],
+            "start_time": data['StartTime'],
+            "end_time": data['EndTime']
+        }
+        schedule.insert_one(schedule_info)
+    result = dict(msg=1)
+    return HttpResponse(json.dumps(result), content_type="application/json")
 
+
+def get_schedule_mongo(request):
+    schedule = mongo_db.schedule
+    for data in schedule.find():
+        print(data)
+    result = dict(msg=1)
+    return HttpResponse(json.dumps(result), content_type="application/json")
+
+
+def delete_schedule_mongo(request):
+    schedule = mongo_db.schedule
+    schedule.delete_many({})
+    result = dict(msg=1)
+    return HttpResponse(json.dumps(result), content_type="application/json")
