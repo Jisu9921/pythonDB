e4937157fa5316e28674d1c7a9313dc08326c31c
jisu choi <h41469921@gmail.com>
2017-08-10 11:52:20 +0900

    Delete mysql test code

--- mysqldb/urls.py
+++ mysqldb/urls.py
@@ -3,7 +3,6 @@ import django.conf.urls
 from .views import *
 
 urlpatterns = [
-    django.conf.urls.url(r'^$', mysql_index, name='mysql_index'),
     django.conf.urls.url(r'^insert_schedule/$', insert_schedule_mysql, name='insert_schedule'),
     django.conf.urls.url(r'^get_schedule/$', get_schedule_mysql, name='get_schedule'),
     django.conf.urls.url(r'^delete_schedule/$', delete_schedule_mysql, name='delete_schedule'),
